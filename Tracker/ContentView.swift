bb2421907160319dc0b291ab42e0edf20af1d791
Elena <elena@cybr.ai>
2024-02-12 16:54:25 +0100

    saving

--- Tracker/ContentView.swift
+++ Tracker/ContentView.swift
@@ -1,80 +1,23 @@
 import SwiftUI
-import SwiftData
-
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-  var body: some View {
-        VStack {
-             Text("Hello World")
-            Button(action: {
-             NavigationLink(destination: WorkoutView()) {
-                    Text("Do Something")
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Hello World")
+                NavigationLink(destination: WorkoutView()) {
+                    Text("Go to WorkoutView")
+                        .font(.largeTitle)
+                        .fontWeight(.ultraLight)
                 }
-            }) {
-                Text("Do Something")
-                    .font(.largeTitle)
-                    .fontWeight(.ultraLight)
             }
         }
     }
-   
-//     var body: some View {
-//         NavigationSplitView 
-//         {
-//             List {
-//                 ForEach(items) { item in
-//                     NavigationLink {
-//                         Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-//                     } label: {
-//                         Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-//                         Text("Hamlet")
-//                             .font(.title)
-//                     }
-//                 }
-//                 .onDelete(perform: deleteItems)
-//             }
-// #if os(macOS)
-//             .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-// #endif
-//             .toolbar {
-// #if os(iOS)
-//                 ToolbarItem(placement: .navigationBarTrailing) {
-//                     EditButton()
-//                 }
-// #endif
-//                 ToolbarItem {
-//                     Button(action: addItem) {
-//                         Label("Add Item", systemImage: "plus")
-                       
-//                     }
-//                 }
-//             }
-//         } detail: {
-//             Text("Select an item")
-//         }
-//     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-        
-    }
-    
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+// Assuming you have a preview provider, it should look something like this:
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ Tracker/workout view.swift
@@ -0,0 +1,8 @@
+//
+//  workout view.swift
+//  Tracker
+//
+//  Created by Elena on 12/02/2024.
+//
+
+import Foundation
